7d59fdcd5f53f29157f3205bb7784d58ee0258b3
Andrii Halchyshak <agalchisak@gmail.com>
2026-03-16 18:05:05 +0200

    style(auth): add decorative yellow stripes and background pattern

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -5,6 +5,13 @@ import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <main className="relative min-h-screen w-full bg-main-black flex flex-col items-center justify-start pt-25 p-6 overflow-hidden"></main>
+    <main className="relative min-h-screen w-full bg-main-black flex flex-col items-center justify-start pt-25 p-6 overflow-hidden">
+      <div className="absolute top-0 right-0 w-64 h-64 overflow-hidden pointer-events-none">
+        <div className="absolute z-10 -top-5 -right-12 w-96 h-3 bg-main-yellow rotate-35" />
+        <div className="absolute z-10 top-1 -right-12 w-96 h-3 bg-main-yellow rotate-35" />
+      </div>
+
+      <div className="absolute inset-0 pointer-events-none bg-cover bg-[url('/bg.png')]" />
+    </main>
   );
 }
