971ef554fe477408cd8ecc9e5ab02cbf86b52455
Andrii Halchyshak <agalchisak@gmail.com>
2026-03-16 18:04:27 +0200

    feat(auth): create basic login page structure

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+    <main className="relative min-h-screen w-full bg-main-black flex flex-col items-center justify-start pt-25 p-6 overflow-hidden"></main>
+  );
+}
