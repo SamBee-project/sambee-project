bb5a9770e3e9a2669cf63134382cb7dc80c1490b
TarasikPy <interestingall24@gmail.com>
2026-04-08 20:01:10 +0000

    feat(auth): core user manager and fastapi-users config

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,14 +1,6 @@
-from sqlalchemy import Boolean, Column, Integer, String
-from sqlalchemy.orm import relationship
-from app.db.base import Base
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from app.db.base_class import Base 
 
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True, nullable=False)
-
-    hives = relationship("Hive", back_populates="user")
+class User(SQLAlchemyBaseUserTableUUID, Base):
+   __tablename__ = "users"
+   pass
